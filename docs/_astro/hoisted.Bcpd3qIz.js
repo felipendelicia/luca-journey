64e29881399c7747b222825a87617829d3d64207
felipendelicia <delicia4581@gmail.com>
2026-06-07 13:59:26 -0300

    safari: encuentro en 2 pasos (carta + selector de balls + minijuego de anillo + escape)

--- docs/_astro/hoisted.Bcpd3qIz.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./tutorial.lzfFi3ez.js";import"./hoisted.zynldspA.js";document.getElementById("ver-tuto")?.addEventListener("click",()=>t(!0));
---
+++ docs/_astro/hoisted.DQ_4931e.js
@@ -0,0 +1,1 @@
+import{i as t}from"./tutorial.lzfFi3ez.js";import"./hoisted.BhMyR5o2.js";document.getElementById("ver-tuto")?.addEventListener("click",()=>t(!0));
