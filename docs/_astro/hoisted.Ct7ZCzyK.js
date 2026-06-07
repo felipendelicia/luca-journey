1d9a633eb8df2bebb155d62b94ca3abbb2667124
felipendelicia <delicia4581@gmail.com>
2026-06-07 16:23:14 -0300

    ui: botones −10 (baya) + Reset EVs en pokedex + bayas/Borrón en la tienda

--- docs/_astro/hoisted.Ct7ZCzyK.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./tutorial.lzfFi3ez.js";import"./hoisted.iaY_Xda4.js";document.getElementById("ver-tuto")?.addEventListener("click",()=>t(!0));
---
+++ docs/_astro/hoisted.DZVauMX1.js
@@ -0,0 +1,1 @@
+import{i as t}from"./tutorial.lzfFi3ez.js";import"./hoisted.D88hgK6u.js";document.getElementById("ver-tuto")?.addEventListener("click",()=>t(!0));
